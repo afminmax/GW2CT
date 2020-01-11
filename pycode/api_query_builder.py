d6c660a01a65a3857546c7857f22ec45fdca7dd9
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-01-11 21:30:00 +0100

    add sp exec and file move at the runs end

--- pycode/api_query_builder.py
+++ pycode/api_query_builder.py
@@ -1,24 +1,27 @@
-print("Starting module imports....")
+print('Starting module imports....')
 
 import pyodbc
+import pymssql
 import json
 import requests
 import datetime, time
 from pathlib import Path
+import shutil
+import os
 
 
-print("Finished loading module imports....")
+print('Finished loading module imports....')
 
-print("Logging into SQL servver....")
-server = 'TABLA\SQLEXPRESS' 
-database = 'gw2cat' 
-username = 'devvm' 
-password = '30032'
+print('Logging into SQL server....')
+server = '*'
+database = '*'
+username = '*'
+password = '*'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-print("SQL servver logged in....")
+print('SQL server logged in.')
 
-print("Creating the API Queries...")
+print('Creating the API Queries...')
 apiKeyDict = {}
 
 ## CREATE THE API QUERIES. OUTPUT GOES TO A DICTIONARY.
@@ -36,29 +39,32 @@ for key,value in matCatsDict.items():
     # print(query_string)
     apiKeyDict.update({key:query_string})
 
-print("Completed the API Queries Dictionary:")
+print('Completed the API Queries Dictionary.')
 # print(apiKeyDict)
-
-
+num_files_to_process = len(apiKeyDict)
 
 # SUBMIT API REQUESTS
-print("Submitting the API Queries to the GW2 API....")
+print('Generating the time stamp data in UTC...')
 
 # get sql iso8601 offset timestamp 
 sql_dts = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-print(sql_dts)
+# print(sql_dts)
 sql_dts_dict = {'dts':sql_dts}
 
 # kluge. redo this later. a second easier to format dts for the file run name
 filename_dts = datetime.datetime.utcnow()
-print(filename_dts)
+print('The timestamp to be used for this run: ' + str(filename_dts))
 
+print('Submitting the API Queries to the GW2 API....')
 # build friendly filename for saved json
 # run_name = 'run_' + filename_dts.strftime("%Y-%m-%d %H.%M.%S") + '_'
-run_name = filename_dts.strftime("%Y-%m-%d %H.%M.%S")
-print("The run name is: " + run_name)
-data_folder = Path("c:\gw2ct\json_get")
+run_name = filename_dts.strftime('%Y-%m-%d %H.%M.%S')
+print('The run name is: ' + run_name)
+data_folder = Path('c:\gw2ct\json_get')
+print('There are ' + str(num_files_to_process) + ' API queries to submit for the current items in gw2ct.')
 
+# for loop to go through each API query string
+file_counter = 0
 for key,value in apiKeyDict.items():
     URL = value
     # print(URL)
@@ -69,14 +75,37 @@ for key,value in apiKeyDict.items():
     file_name = run_name + '_' + matCatsDict[key] + '_prices.json'
     with open(data_folder/file_name, 'w') as outfile:
         json.dump(data, outfile, indent=4)
+    file_counter = file_counter + 1
+    print('Completed ' + str(file_counter) + ' of ' + str(num_files_to_process) + ' queries.')
 
-print("The queries have been submitted and written to disk. Ready for SQL!")
+print('The queries have been submitted and written to disk. Ready for SQL loading!')
+print('SQL... let us begin!')
+
+sql_server = '*' 
+sql_database = '*' 
+sql_username = '*' 
+sql_password = '*'
+
+conn = pymssql.connect(server=sql_server, user=sql_username, password=sql_password, database=sql_database)
+sqlcursor = conn.cursor()
+print('SQL... Initiating the stored procedure.....')
+sqlcursor.callproc('spApiJsonSqlLoad')
+conn.commit()
+print('Voila.... fucking done!')
+conn.close()
+
+print('Moving files....')
+source = 'C:\gw2ct\json_get'
+dest1 = 'D:\gw2ct archive'
+files = os.listdir(source)
+for f in files:
+        shutil.move(source+'\\'+f, dest1)
+print('The file move is complete. This job run has completed. Thank you, have a nice day. :-D ')
 
 
-
-
-
-
+# import pandas as pd
+# results = pd.DataFrame(cursor.execute("{call spGetAllItems}"))
+# print(results)
 
 # to do:
 # 0. loop to run all API queries - DONE
